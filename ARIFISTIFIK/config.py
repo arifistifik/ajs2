0829a9fce80f93f41345c17eaa2b8604070de561
ARIF MH <37590868+arifistifik@users.noreply.github.com>
2018-09-02 02:49:21 +0700

    Update config.py

--- ARIFISTIFIK/config.py
+++ ARIFISTIFIK/config.py
@@ -28,9 +28,9 @@ class Config(object):
     }
 
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
-    APP_VER     = '0.0.2'
+    APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'ARIF-PC'
+    SYSTEM_NAME = 'ARIFISTIFIK'
     SYSTEM_VER  = '11.2.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
